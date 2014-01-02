1c000f42807cc04f8ff4917373b7e1137801850a
Maciej Piechotka <uzytkownik2@gmail.com>
2014-01-02 14:40:03 +0100

    Add macro DSL for testing

--- test.rs
+++ test.rs
@@ -20,16 +20,47 @@
  * THE SOFTWARE.
  */
 
+#[feature(macro_rules)];
+
 extern mod xml;
 
-#[test]
-fn test_simple_parse() {
-    let xml = "<?xml version=\"1.0\"?> <test />".as_bytes();
-    let doc = xml::read_memory(xml).unwrap();
-    let root = doc.get_root_element().unwrap();
-    assert_eq!(root.name(), ~"test");
-    assert!(root.children_iter().next().is_none());
-}
+macro_rules! expect_root_elem(
+    ($root:expr, $iter:ident, $expected_name:expr, $elem_check:expr) => ({
+        assert_eq!($root.name(), $expected_name);
+        {
+            let mut $iter = $root.children_iter();
+            $elem_check;
+            assert!($iter.next().is_none(), "Unexpected children");
+        }
+    })
+)
+
+macro_rules! expect_elem(
+    ($iter:ident, $expected_name:expr, $elem_check:expr) => ({
+        let next = ($iter).next();
+        assert!(next.is_some(), "Expected element but there is no further element");
+        let elem = next.unwrap().get_element();
+        assert!(elem.is_some(), "Extected element but got other children");
+        let cur = elem.unwrap();
+        {
+            let mut $iter = cur.children_iter();
+            $elem_check;
+            assert!($iter.next().is_none(), "Unexpected children");
+        }
+    })
+)
+
+
+macro_rules! expect_text(
+    ($iter:ident, $expected_text:expr) => ({
+        let next = ($iter).next();
+        assert!(next.is_some(), "Expected text but there is no further element");
+        let text = next.unwrap().get_text();
+        assert!(text.is_some(), "Extected text but got other children");
+        let cur = text.unwrap();
+        assert_eq!(cur.content(), $expected_text);
+    });
+)
 
 #[test]
 fn test_simple_fail() {
@@ -39,39 +70,26 @@ fn test_simple_fail() {
 }
 
 #[test]
+fn test_simple_parse() {
+    let xml = "<?xml version=\"1.0\"?> <test />".as_bytes();
+    let doc = xml::read_memory(xml).unwrap();
+    let root = doc.get_root_element().unwrap();
+    expect_root_elem!(root, iter, ~"test", {});
+}
+
+#[test]
 fn test_subelements() {
     let xml = "<?xml version=\"1.0\"?> <test> <a> <b></b>aaa</a><c/></test>".as_bytes();
     let doc = xml::read_memory(xml).unwrap();
     let root = doc.get_root_element().unwrap();
-    assert_eq!(root.name(), ~"test");
-    let mut iter = root.children_iter();
-    {
-        let cur = iter.next().unwrap().get_text().unwrap();
-        assert_eq!(cur.content(), ~" ");
-    }
-    {
-        let cur = iter.next().unwrap().get_element().unwrap();
-        assert_eq!(cur.name(), ~"a");
-        let mut iter = cur.children_iter();
-        {
-            let cur = iter.next().unwrap().get_text().unwrap();
-            assert_eq!(cur.content(), ~" ");
-        }
-        {
-            let cur = iter.next().unwrap().get_element().unwrap();
-            assert_eq!(cur.name(), ~"b");
-            assert!(cur.children_iter().next().is_none());
-        }
-        {
-            let cur = iter.next().unwrap().get_text().unwrap();
-            assert_eq!(cur.content(), ~"aaa");            
-        }
-        assert!(iter.next().is_none());
-    }
-    {
-        let cur = iter.next().unwrap().get_element().unwrap();
-        assert_eq!(cur.name(), ~"c");
-        assert!(cur.children_iter().next().is_none());
-    }
-    assert!(iter.next().is_none());
+    expect_root_elem!(root, iter, ~"test", {
+        expect_text!(iter, ~" ");
+        expect_elem!(iter, ~"a", {
+            expect_text!(iter, ~" ");
+            expect_elem!(iter, ~"b", {});
+            expect_text!(iter, ~"aaa");
+        });
+        expect_elem!(iter, ~"c", {});
+    });
 }
+
